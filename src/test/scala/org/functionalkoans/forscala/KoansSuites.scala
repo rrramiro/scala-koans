156367d5c6d99fe9cd5c83898707a0fc1209caca
CALLE Ramiro <6574825+rrramiro@users.noreply.github.com>
2016-07-20 16:57:38 +0100

    Enable word wrapping for the message

--- src/test/scala/org/functionalkoans/forscala/KoansSuites.scala
+++ src/test/scala/org/functionalkoans/forscala/KoansSuites.scala
@@ -6,27 +6,34 @@ import scala.language.reflectiveCalls
 
 abstract class KoansSuites(suitesToNest: Suite*) extends Suites(suitesToNest: _*){
   private class ReportToTheMaster(other: Reporter, stopper: Stopper) extends Reporter {
+
+    private val COL_SIZE = 42
+
     private type KoanEven = {
       val ordinal : Ordinal
       val suiteName: String
       val testName: String
     }
+
+    def wordWrap(s: String, maxLength: Int): String = {
+      s.split(" ").foldLeft(Array(""))( (out, in) => {
+        if ((out.last + " " + in).trim.length > maxLength) out :+ in
+        else out.updated(out.length - 1, out.last + " " + in)
+      }).mkString("\n").trim
+    }
+
     private def failure(event: KoanEven) =  {
       stopper.requestStop
       InfoProvided(
         ordinal = event.ordinal,
         message = Seq(
-          "*****************************************",
-          "*****************************************",
+          "*" * COL_SIZE,
+          "*" * COL_SIZE,
           "",
+          wordWrap("Please meditate on koan \"%s\" of suite \"%s\"" format(event.testName, event.suiteName), COL_SIZE),
           "",
-          "",
-          "Please meditate on koan \"%s\" of suite \"%s\"" format(event.testName, event.suiteName),
-          "",
-          "",
-          "",
-          "*****************************************",
-          "*****************************************"
+          "*" * COL_SIZE,
+          "*" * COL_SIZE
         ).mkString("\n", "\n", ""),
         nameInfo = Some(NameInfo("Koan", "koan", None,None))
       )
